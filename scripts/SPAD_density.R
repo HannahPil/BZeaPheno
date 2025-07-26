bf48ca97bfa93c150685c1dbbd45b81c4c87ba94
HannahPil <pilhannahd@gmail.com>
2025-07-26 12:24:34 -0400

    ridge

--- scripts/SPAD_density.R
+++ scripts/SPAD_density.R
@@ -3,7 +3,7 @@ library(ggthemes)
 library(dplyr)
 library(agricolae)
 library(multcompView)
-
+library(ggridges)
 
 setwd("C:/Users/Hannah Pil/Documents/gemmalab/BZea/R/phenotyping_figures/data")
 BZea <- read.csv("D4_SPAD_edited.csv")
@@ -75,6 +75,7 @@ print(overlap_plotty)
 
 
 #----------2025 SPAD---------------------------------------------------
+
 library(ggplot2)
 library(ggthemes)
 library(dplyr)
@@ -83,28 +84,25 @@ library(multcompView)
 
 
 setwd("C:/Users/Hannah Pil/Documents/gemmalab/BZea/BZea phenotyping/BZeaPheno")
-BZea <- read.csv("SPAD-CLY25-Fieldbook - B5_BZea_eval.csv")
+BZea_df <- read.csv("SPAD-CLY25-Fieldbook - B5_BZea_eval.csv")
 
-BZea$SPAD <- as.numeric(BZea$SPAD)
+BZea_df$SPAD <- as.numeric(BZea_df$SPAD)
+BZea_df$species_mex <- factor(BZea_df$species_mex, levels = c("Mex", "Hueh", "Bals", "Zlux", "Zdip", "B73"))
 
 #set order and color
 species_order <- c("Mex", "B73", "Bals", "Zdip", "Hueh", "Zlux")
 species_colors <- c("#5f9bfe", "#02bec5", "#f463e2", "#f8756d", "#b69c00", "#00b836")
 
-#set order and color
-species_3 <- c("B73", "Zdip", "Zlux")
-species_3_colors <- c("white", "#e4211c", "#367db8")
 
 # exclude checks
-BZea_df <- BZea %>%
-  filter(!Species %in% c("Check"))
+BZea_df <- BZea_df %>%
+  filter(!species_mex %in% c("Check", ""))
 
-         
 #order
-BZea_df$Species <- factor(BZea_df$Species, levels = species_order)
+BZea_df$Species <- factor(BZea_df$species, levels = species_order)
 
 #plot with all SPAD1s overlapping
-SPAD1_plotty <- ggplot(BZea_df, aes(x = SPAD1, fill=Species)) +
+SPAD1_plotty <- ggplot(BZea_df, aes(x = SPAD, fill=species_mex)) +
   geom_density(alpha = 0.2) +
   theme_minimal() +
   scale_fill_manual(values = species_colors) +
@@ -114,38 +112,26 @@ SPAD1_plotty <- ggplot(BZea_df, aes(x = SPAD1, fill=Species)) +
 print(SPAD1_plotty)
 
 
-#plot with B73, zdip, zlux SPAD1s overlapping
-SPAD1_3 <- ggplot(BZea_df_3, aes(x = SPAD1, fill=Species)) +
-  geom_density(alpha = 0.5, size=.5) +
-  theme_minimal() +
-  scale_fill_manual(values = species_3_colors) +
-  scale_x_continuous(limits = c(0, 80)) +
-  labs(x="Leaf Greenness Index", y="Density")
+#------2025 ridge---------------------------------
 
-print(SPAD1_3)
+#rainbow overlapping a little
+ggplot(BZea_df, aes(x = SPAD, y = species_mex, fill = species_mex, height = stat(density))) + 
+  geom_density_ridges(scale = 3, draw_baseline = FALSE, alpha=0.7)
 
-#plot with B73, zdip, zlux SPAD2s overlapping
-SPAD1_32 <- ggplot(BZea_df_3, aes(x = SPAD2, fill=Species)) +
-  geom_density(alpha = 0.5, size=.5) +
-  theme_minimal() +
-  scale_fill_manual(values = species_3_colors) +
-  scale_x_continuous(limits = c(0, 80)) +
-  labs(x="Leaf Greenness Index", y="Density")
-
-print(SPAD1_32)
+#quantiles
+ggplot(BZea_df, aes(x=SPAD, y=species_mex, fill = factor(stat(quantile)))) +
+  stat_density_ridges(
+    geom = "density_ridges_gradient", calc_ecdf = TRUE,
+    quantiles = 4, quantile_lines = TRUE
+  ) +
+  scale_fill_viridis_d(name = "Quartiles")
 
 
-
-#plot SPAD1 and 2 overlapped but species separate
-overlap_plotty <- ggplot(BZea_df, aes(x = SPAD1, fill = "SPAD1")) +
-  geom_density(alpha = 0.5) +
-  geom_density(aes(x = SPAD2, fill = "SPAD2"), alpha = 0.5, linetype = 2) +
-  facet_wrap(~Species, scales = "free_x", ncol = 6) +
-  theme_minimal() +
-  scale_fill_manual(values = c("#00b836", "#b69c00"),
-                        name = "",
-                        labels = c("SPAD1", "SPAD2")) +
-  scale_x_continuous(limits = c(0, 80)) +
-  labs(x="Leaf Greenness Index", y="Density")
-
-print(overlap_plotty)
+ggplot(BZea_df, aes(x = SPAD, y = species_mex, fill = species_mex)) +
+  geom_density_ridges(
+    aes(point_color = species_mex, point_fill = species_mex, point_shape = species_mex),
+    alpha = .2, point_alpha = 1, jittered_points = TRUE
+  ) +
+  scale_point_color_hue(l = 40) +
+  scale_discrete_manual(aesthetics = "point_shape", values = c(21, 21, 21, 21, 21, 21)) +
+  labs(x = "Leaf greenness index", y = "Species")
